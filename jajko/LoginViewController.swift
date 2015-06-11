c49d790a78de516178aac977a264fc876d2338d2
Esteban Garro <egarro@transcriptics.com>
2015-06-11 09:38:09 -0400

    Initial commit. Includes a login screen and a signup controller

---
+++ jajko/LoginViewController.swift
@@ -0,0 +1,50 @@
+//
+//  LoginViewController.swift
+//  jajko
+//
+//  Created by Esteban Garro on 2015-06-11.
+//  Copyright (c) 2015 transcriptics. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func login(sender: AnyObject) {
+        if doLogin() {
+        
+        } else {
+        
+        }
+    }
+    
+    @IBAction func cancelToLoginViewController(segue:UIStoryboardSegue) {
+    }
+    
+    @IBAction func savePlayerDetail(segue:UIStoryboardSegue) {
+        
+    }
+    
+    func doLogin() -> Bool {
+        var didLogin = false
+        let username = self.usernameField.text
+        let password = self.passwordField.text
+        
+        
+        return didLogin
+    }
+}
+
---
+++ jajko/SignUpViewController.swift
@@ -0,0 +1,43 @@
+//
+//  SignUpViewController.swift
+//  jajko
+//
+//  Created by Esteban Garro on 2015-06-11.
+//  Copyright (c) 2015 transcriptics. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+    
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func login(sender: AnyObject) {
+        if doLogin() {
+            
+        } else {
+            
+        }
+    }
+    
+    func doLogin() -> Bool {
+        var didLogin = false
+        let username = self.usernameField.text
+        let password = self.passwordField.text
+        
+        
+        return didLogin
+    }
+}
+
