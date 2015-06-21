78a24d0434017082d37a4f8236f6045b1fa2cb05
Esteban Garro <egarro@transcriptics.com>
2015-06-21 09:24:11 -0400

    Adds messages to the Login screen

--- jajko/LoginViewController.swift
+++ jajko/LoginViewController.swift
@@ -12,6 +12,11 @@ class LoginViewController: UIViewController, InputDelegate {
 
     @IBOutlet weak var usernameField: TextBox!
     @IBOutlet weak var passwordField: TextBox!
+    
+    @IBOutlet weak var myIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    @IBOutlet weak var signupButton: UIButton!
     @IBOutlet weak var containerView: UIScrollView!
     
     override func viewDidLoad() {
@@ -57,8 +62,19 @@ class LoginViewController: UIViewController, InputDelegate {
         return didLogin
     }
 
+// MARK: Utility methods
+    func show(message: String) {
+        messageLabel.text = message
+        messageLabel.hidden = false
+    }
+    func hideMessage() {
+        messageLabel.text = ""
+        messageLabel.hidden = true
+    }
+    
 // MARK: InputDelegate methods
     func didBeginEditing(obj:Taggable) {
+        hideMessage()
         if obj.Tag() == 10 {
             self.containerView.setContentOffset(CGPointMake(0.0, 30.0), animated: true)
         } else {
@@ -67,6 +83,27 @@ class LoginViewController: UIViewController, InputDelegate {
     }
     func didEndEditing(obj:Taggable) {
         self.containerView.setContentOffset(CGPointMake(0.0, 0.0), animated: true)
+    }
+    
+    override func shouldPerformSegueWithIdentifier(identifier: String?, sender: AnyObject?) -> Bool {
+        if (identifier == "startSignup") {
+            let username = self.usernameField.text
+            if username != nil && username != "" // && isValidEmail(username)
+            {
+                //Check if email is not taken, etc...
+            } else {
+                show("You must enter a valid email address as username in order to signup!")
+                return false
+            }
+        }
+
+        return true
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject!) {
+        if (segue.identifier == "startSignup") {
+            // pass data to next view
+        }
     }
 }
 
