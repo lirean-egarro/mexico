44e5c46e0862591dfd28484a667b04da2c018dbc
Esteban Garro <egarro@transcriptics.com>
2015-06-26 22:33:52 -0400

    Completes the main trial audio view

--- jajko/TrialViewController.swift
+++ jajko/TrialViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class TrialViewController: UIViewController, ExperienceReceiver {
     
+    @IBOutlet weak var wordLabel: UILabel!
+    @IBOutlet weak var progressLabel: UILabel!
+    @IBOutlet weak var playButton: UIButton!
+    
+    @IBOutlet weak var leftButton: UIButton!
+    @IBOutlet weak var rightButton: UIButton!
+    
     var experience:Experience!
     
     required init(coder aDecoder: NSCoder) {
@@ -22,7 +29,14 @@ class TrialViewController: UIViewController, ExperienceReceiver {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        var bg = UIImage(named: "WhiteTile")?.resizableImageWithCapInsets(UIEdgeInsetsMake(7, 7, 7, 7))
+        self.leftButton.setBackgroundImage(bg, forState: .Normal)
+        self.rightButton.setBackgroundImage(bg, forState: .Normal)
     }
     
+    @IBAction func play(sender:AnyObject?) {
+        println("Should play file")
+    }
 }
 
