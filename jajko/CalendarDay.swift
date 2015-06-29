491b63723121c341dcd9668eff1b0a86547699f5
Esteban Garro <egarro@transcriptics.com>
2015-06-29 03:24:36 -0400

    Completes ScoreView layout.

---
+++ jajko/CalendarDay.swift
@@ -0,0 +1,99 @@
+//
+//  CalendarDay.swift
+//  jajko
+//
+//  Created by Esteban Garro on 2015-06-29.
+//  Copyright (c) 2015 transcriptics. All rights reserved.
+//
+
+import UIKit
+
+let CALENDAR_FONT_SIZE:CGFloat = 14.0
+let CALENDAR_DOT_SIZE:CGFloat = 6.0
+
+protocol CalendarDayDelegate : NSObjectProtocol {
+    func calandarDayDidTurnOn(id:Int)
+}
+
+class CalendarDay: UIView {
+    static var id:Int = 0
+    
+    var mainLabel:UILabel!
+    var hasInfo:Bool
+    weak var delegate:CalendarDayDelegate?
+    var udid:Int
+    var isOn:Bool {
+        willSet {
+            self.setNeedsDisplay()
+        }
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        fatalError("Calendar day must be instantiated from code.")
+    }
+    
+    override init(frame: CGRect) {
+        hasInfo = false
+        isOn = false
+        udid = CalendarDay.id++
+        super.init(frame: frame)
+    }
+    
+    convenience init(frame:CGRect, title: String?, hasInfo:Bool, isSelectable:Bool) {
+        self.init(frame: frame)
+        
+        var labelHeight = CALENDAR_FONT_SIZE + 3.0
+        mainLabel = UILabel(frame: CGRectMake(0.0, (frame.size.height - labelHeight)/2.0 - CALENDAR_DOT_SIZE, frame.size.width
+            , labelHeight))
+        mainLabel.textColor = UIColor.whiteColor()
+        mainLabel.font = ControlsConfig.textFontOfSize(CALENDAR_FONT_SIZE)
+        mainLabel.textAlignment = .Center
+        mainLabel.text = title
+        
+        
+        self.hasInfo = hasInfo
+
+        //Detect the tap on the view to trigger the check/uncheck events:
+        if isSelectable {
+            let recognizer = UITapGestureRecognizer(target: self, action:Selector("handleTap:"))
+            self.addGestureRecognizer(recognizer)
+        }
+        
+        self.backgroundColor = UIColor.clearColor()
+        self.contentMode = UIViewContentMode.Redraw
+    }
+    
+    override func layoutSubviews() {
+        self.addSubview(mainLabel)
+    }
+    
+    override func drawRect(rect: CGRect) {        
+        if isOn {
+            UIColor.whiteColor().setFill();
+            CGContextFillRect(UIGraphicsGetCurrentContext(), rect);
+            mainLabel.textColor = UIColor.darkGrayColor()
+            ControlsConfig.generalGreenColor().set()
+        } else {
+            UIColor.clearColor().setFill();
+            CGContextFillRect(UIGraphicsGetCurrentContext(), rect);
+            mainLabel.textColor = UIColor.whiteColor()
+            UIColor.whiteColor().set()
+        }
+        
+        if hasInfo {
+            var circleRect = CGRectMake((self.frame.size.width-CALENDAR_DOT_SIZE)/2.0,(self.frame.size.height-CALENDAR_DOT_SIZE)/2.0 + CALENDAR_DOT_SIZE,CALENDAR_DOT_SIZE,CALENDAR_DOT_SIZE)
+            var myBezier = UIBezierPath(ovalInRect:circleRect)
+            myBezier.fill()
+        }
+    }
+    
+// MARK: Utility Methods
+    func handleTap(recognizer: UITapGestureRecognizer) {
+        if isOn {
+            self.isOn = false
+        } else {
+            self.isOn = true
+            self.delegate?.calandarDayDidTurnOn(self.udid)
+        }
+    }
+}
--- jajko/ScoresViewController.swift
+++ jajko/ScoresViewController.swift
@@ -8,9 +8,12 @@
 
 import UIKit
 
-class ScoresViewController: UIViewController {
+let daysOfWeek =  ["SUN","MON","TUE","WED","THU","FRI","SAT"]
+
+class ScoresViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, CalendarDayDelegate {
     
     @IBOutlet weak var dismissButton: UIButton!
+    @IBOutlet weak var calendar:UIView!
     
     required init(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
@@ -23,6 +26,64 @@ class ScoresViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+
+        let dayWidth = calendar.frame.size.width / 7.0
+        let yOffset = calendar.frame.size.height / 7.0
+        let dayHeight = yOffset
+        
+        //SPECIFICS FOR JULY CALENDAR:
+        for var r = 0 ; r < 6 ; r++ {
+            for var d = 0 ; d < 7 ; d++ {
+                var tmp:CalendarDay?
+                var dayRect = CGRectMake(CGFloat(d)*dayWidth,yOffset+CGFloat(r)*dayHeight - 10.0,dayWidth,dayHeight)
+                if r == 0 {
+                    tmp = CalendarDay(frame: dayRect, title: daysOfWeek[d], hasInfo: false, isSelectable: false)
+                } else if r == 1 && d > 2 {
+                    tmp = CalendarDay(frame: dayRect, title: String(d-2), hasInfo: false, isSelectable: true)
+                } else if r == 5 && d < 3 {
+                    tmp = CalendarDay(frame: dayRect, title: String(28+d), hasInfo: true, isSelectable: true)
+                } else if r > 1 && r < 5 {
+                    tmp = CalendarDay(frame: dayRect, title: String((r-1)*7+d), hasInfo: false, isSelectable: true)
+                }
+                
+                if tmp != nil {
+                    calendar.addSubview(tmp!)
+                    tmp!.delegate = self
+                }
+            }
+        }
+        
+    }
+ 
+// MARK: CalendarDayDelegateMethods
+    func calandarDayDidTurnOn(id:Int) {
+        for tmp in calendar.subviews {
+            if tmp.isKindOfClass(CalendarDay) && (tmp as! CalendarDay).udid != id {
+                (tmp as! CalendarDay).isOn = false
+            }
+        }
+    }
     
+// MARK: UITableViewController Delegate Methods
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        var cell:UITableViewCell = tableView.dequeueReusableCellWithIdentifier("simpleCell") as! UITableViewCell
+        
+        cell.textLabel?.text = "Hello"
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+    }
 }
 
